ca6493d0923e1de07d2e4270792c7b628b2f6733
Bruna Flor <brn.flor@gmail.com>
2022-11-23 17:54:42 +0000

    header component styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,53 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
+import { Banner } from "./App.styles";
+import Logo from "./assets/LogoCoffeDelivery.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
+import BannerImg from "./assets/CoffeBanner.png";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Header />
-      <div>
-        <main>
-          <div>Banner</div>
+      <main>
+        <Banner>
           <div>
-            <div>Card</div>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <h3>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </h3>
+            <div>
+              <div>
+                <div>
+                  <span>icon</span>
+                  <p>text</p>
+                </div>
+                <div>
+                  <span>icon</span>
+                  <p>text</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <span>icon</span>
+                  <p>text</p>
+                </div>
+                <div>
+                  <span>icon</span>
+                  <p>text</p>
+                </div>
+              </div>
+            </div>
           </div>
-        </main>
-      </div>
+          <img src={BannerImg} />
+        </Banner>
+        <div>
+          <div>Card</div>
+        </div>
+      </main>
     </ThemeProvider>
   );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const NavBar = styled.nav`
   display: flex;
-  align-items: center;
-  /* justify-content: space-between; */
-  padding: 2rem;
-
+  justify-content: space-between;
+  padding: 2rem 0;
+  margin: 0 10rem;
   div {
     display: flex;
     gap: 1rem;
+  }
+  img {
   }
 `;
 
@@ -24,28 +25,29 @@ export const LocationTag = styled.a`
     color: ${(props) => props.theme.purple};
   }
 `;
+
 export const Cart = styled.a`
-position: relative;
+  position: relative;
 
-display: flex;
-align-items: center;
-background: ${props => props.theme["yellow-light"]};
-color: ${props => props.theme["yellow-dark"]};
-padding: 0.5rem 0.75rem;
-:hover{
-    /* background: ${props => props.theme.yellow}; */
+  display: flex;
+  align-items: center;
+  background: ${(props) => props.theme["yellow-light"]};
+  color: ${(props) => props.theme["yellow-dark"]};
+  padding: 0.5rem 0.75rem;
+  :hover {
+    /* background: ${(props) => props.theme.yellow}; */
     cursor: pointer;
-}
+  }
 
-span { 
+  span {
     position: absolute;
     z-index: 1;
     top: -12px;
     right: -10px;
-    background: ${props => props.theme["yellow-dark"]};
-    color:${props => props.theme.white};
+    background: ${(props) => props.theme["yellow-dark"]};
+    color: ${(props) => props.theme.white};
     padding: 0.15rem 0.5rem;
     /* line-height: 0; */
     border-radius: 50%;
-}
-`
+  }
+`;
