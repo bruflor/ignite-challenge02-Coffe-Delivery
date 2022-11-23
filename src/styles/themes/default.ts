cb650e70688c15c75eda555234b8fae28abf6ef6
Bruna Flor <brn.flor@gmail.com>
2022-11-23 14:51:10 +0000

    default and global styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: "#ffffff",
+  background: "#fafafa",
+
+  "yellow-dark": "#c47f17",
+  "yellow-light": "#f1e9c9",
+
+  purple: "#8047f8",
+  "purple-dark": "#4b2995",
+  "purple-light": "#ebe5f9",
+
+  "base-title": "#272221",
+  "base-subtitle": "#403937",
+  "base-text": "#574f4d",
+  "base-label": "#8d8686",
+  "base-hover": "#d7d5d5",
+  "base-button": "#e6e5e5",
+  "base-input": "#ededed",
+  "base-card": "#f3f2f2",
+  'gray-900': '#121214',
+
+};
