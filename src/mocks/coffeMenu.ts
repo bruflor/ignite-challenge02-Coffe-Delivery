8369bdd31fb465d0f5d698ce4b7c18a257ee5353
Bruna Flor <brn.flor@gmail.com>
2022-11-24 11:22:21 +0000

    mock data

---
+++ src/mocks/coffeMenu.ts
@@ -0,0 +1,89 @@
+export const coffeMenu = [
+  {
+    name: "Expresso Tradicional",
+    definition: "O tradicional café feito com água quente e grãos moídos",
+    tags: ["tradicional"],
+    price: 9.9,
+  },
+  {
+    name: "Expresso Americano",
+    definition: "Expresso diluído, menos intenso que o tradicional",
+    tags: ["tradicional"],
+    price: 9.9,
+  },
+  {
+    name: "Expresso Cremoso",
+    definition: "Café expresso tradicional com espuma cremosa",
+    tags: ["tradicional"],
+    price: 9.9,
+  },
+  {
+    name: "Expresso Gelado",
+    definition: "Bebida preparada com café expresso e cubos de gelo",
+    tags: ["tradicional"],
+    price: 9.9,
+  },
+  {
+    name: "Café com Leite",
+    definition: "Meio a meio de expresso tradicional com leite vaporizado",
+    tags: ["tradicional", "com leite"],
+    price: 9.9,
+  },
+  {
+    name: "Latte",
+    definition:
+      "Uma dose de café expresso com o dobro de leite e espuma cremosa",
+    tags: ["tradicional", "com leite"],
+    price: 9.9,
+  },
+  {
+    name: "Capuccino",
+    definition:
+      "Bebida com canela feita de doses iguais de café, leite e espuma",
+    tags: ["tradicional", "com leite"],
+    price: 9.9,
+  },
+  {
+    name: "Macchiato",
+    definition: "Café expresso misturado com um pouco de leite quente e espuma",
+    tags: ["tradicional", "com leite"],
+    price: 9.9,
+  },
+  {
+    name: "Mocaccino",
+    definition: "Café expresso com calda de chocolate, pouco leite e espuma",
+    tags: ["tradicional", "com leite"],
+    price: 9.9,
+  },
+  {
+    name: "Chocolate Quente",
+    definition: "Bebida feita com chocolate dissolvido no leite quente e café",
+    tags: ["tradicional"],
+    price: 9.9,
+  },
+  {
+    name: "Cubano",
+    definition:
+      "Drink gelado de café expresso com rum, creme de leite e hortelã",
+    tags: ["especial", "alcoólico", "gelado"],
+    price: 9.9,
+  },
+  {
+    name: "Havaiano",
+    definition: "Bebida adocicada preparada com café e leite de coco",
+    tags: ["especial"],
+    price: 9.9,
+  },
+  {
+    name: "Árabe",
+    definition: "Bebida preparada com grãos de café árabe e especiarias",
+    tags: ["especial"],
+    price: 9.9,
+  },
+  {
+    name: "Irlandês",
+    definition: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
+    tags: ["especial", "alcoólico"],
+    price: 9.9,
+  },
+];
