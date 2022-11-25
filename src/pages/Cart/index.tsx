bceabc51e766096d0dbf0443955828b5654a7e39
Bruna Flor <brn.flor@gmail.com>
2022-11-25 10:58:44 +0000

    wip: form

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,31 @@
-import { CartPageContainer } from "./style";
+import { MapPinLine } from "phosphor-react";
+import { CartPageContainer, FormContainer } from "./style";
 
 export const Cart = () => {
   return (
     <CartPageContainer>
-      <div>Form + payment</div>
+      <div>
+        <FormContainer>
+          <div>
+            <MapPinLine size={24} />
+            <div>
+              <h3>Endereço de Entrega</h3>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </div>
+          <form>
+            <input placeholder="CEP" />
+            <input placeholder="Rua" />
+            <input placeholder="Número" />
+            <input placeholder="Complemento" />
+            <input placeholder="Bairro" />
+            <div>
+              <input placeholder="Cidade" />
+              <input placeholder="UF" />
+            </div>
+          </form>
+        </FormContainer>
+      </div>
       <div>Cards products + total items and button</div>
     </CartPageContainer>
   );
--- src/pages/Cart/style.ts
+++ src/pages/Cart/style.ts
@@ -8,10 +8,76 @@ export const CartPageContainer = styled.div`
   /* align-items: center; */
 
   div {
-    background: red;
+    /* background: red; */
   }
 
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
+  }
+`;
+
+export const FormContainer = styled.div`
+  background: ${(props) => props.theme["base-card"]};
+  padding: 3rem;
+  > div {
+    display: flex;
+    margin-bottom: 2rem;
+
+    > div > h3 {
+      font-size: 1rem;
+    }
+    > div > span {
+      font-size: 0.875rem;
+    }
+  }
+  form {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 0.5rem;
+    > div {
+      grid-column: -1 / 2;
+      display: flex;
+      gap: 0.5rem;
+    }
+    input {
+      width: 100%;
+      background: ${(props) => props.theme["base-input"]};
+      border: 1px solid ${(props) => props.theme["base-button"]};
+      border-radius: 4px;
+      outline: none;
+      padding: 0.5rem;
+      :focus {
+        border: 1px solid ${(props) => props.theme["yellow-dark"]};
+      }
+      :nth-child(2) {
+        grid-column: -1 / 1;
+      }
+      :nth-child(4) {
+        grid-column: -1 / 2;
+      }
+      :last-child {
+        width: 30%;
+      }
+    }
+    @media (max-width: 768px) {
+      grid-template-columns: 1fr;
+      > div {
+        display: block;
+        grid-column: 1 / 3;
+      }
+      input {
+        grid-column: 1 / 3;
+        :nth-child(2) {
+          grid-column: 1 / 3;
+        }
+        :nth-child(4) {
+          grid-column: 1 / 3;
+        }
+        :last-child {
+          margin-top: 0.5rem;
+          width: 100%;
+        }
+      }
+    }
   }
 `;
