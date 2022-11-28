c1ea015945e030acc6db792b8e2d7dc9edc14371
Bruna Flor <brn.flor@gmail.com>
2022-11-28 17:00:02 +0000

    disabled button if is 0

--- src/components/Cards/AmountCounter.tsx
+++ src/components/Cards/AmountCounter.tsx
@@ -2,9 +2,14 @@ import { Minus, Plus } from "phosphor-react";
 import { CounterContainer } from "./style";
 
 export const AmountCounter = ({ coffeAmount, setCoffeAmount }: any) => {
+  const disableButtons = coffeAmount === 0 ? true : false;
+
   return (
     <CounterContainer>
-      <button onClick={() => setCoffeAmount(coffeAmount - 1)}>
+      <button
+        disabled={disableButtons}
+        onClick={() => setCoffeAmount(coffeAmount - 1)}
+      >
         <Minus size={18} weight="fill" />
       </button>
       <span>{coffeAmount}</span>
