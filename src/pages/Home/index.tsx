4c69445ff509d228b12c2d9333cba804502e5a1e
Bruna Flor <brn.flor@gmail.com>
2022-11-29 11:14:10 +0000

    updating cart if the id coffe is the same as previous products

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,33 @@
 import { useContext } from "react";
 import { BannerInfo } from "../../components/Banner";
 import { MenuCard } from "../../components/Cards/menuCard";
+import { ProductCard } from "../../components/Cards/style";
 import { CartContext } from "../../contexts/CartContext";
 import { coffeMenu } from "../../mocks/coffeMenu";
 import { CoffeMenu } from "./style";
 
 export const Home = () => {
   const { onCart, setOnCart } = useContext(CartContext);
+
   function onHandleAddToCart(newItemCart: any) {
-    if (onCart.length >= 1) {
+    const onCartIdEqualToNew = onCart.filter((card) => {
+      return card.id === newItemCart.id;
+    });
+
+    if (onCart.length >= 1 && onCartIdEqualToNew.length <= 0) {
       setOnCart([...onCart, newItemCart]);
+    } else if (onCart.length >= 1 && onCartIdEqualToNew.length >= 1) {
+      const productsOnCart = [...onCart];
+      const coffeIndexToUpdate = productsOnCart.findIndex(
+        (coffe) => coffe.id === onCartIdEqualToNew[0].id
+      );
+      productsOnCart[coffeIndexToUpdate].amount = newItemCart.amount;
+      setOnCart(productsOnCart);
     } else {
       setOnCart([newItemCart]);
     }
   }
+
   return (
     <main>
       <BannerInfo />
